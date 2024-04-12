b83343203d64a3d1f62b2b041224f566173aef59
lilian <lilian.do.khac@rwth-aachen.de>
2024-04-12 08:55:58 +0000

    .

--- morerichdata/entityrecognition.py
+++ morerichdata/entityrecognition.py
@@ -1,12 +1,30 @@
 import os
+import fire
+import pandas as pd
+import ollama
 from textblob import TextBlob
 from tika import parser
-
 path = "../data/"
 dir_list = os.listdir(path)
-element = 0
+element = 1
+text = parser.from_file(f'{path}/{dir_list[element]}')
+df = pd.DataFrame(text)
+print(df)
 
-text = parser.from_file(f'../data/{dir_list[element]}')
-blob = TextBlob(text["content"])
+ollama.pull('zeffmuks/universal-ner')
+print("starting gen")
 
-entities = blob.noun_phrases
+stream = ollama.chat(
+    model='zeffmuks/universal-ner',
+    messages=[
+        {'role': 'system', 'content': 'A virtual assistant answers questions from a user based on the provided text.'},  
+        {'role': 'user', 'content': f'Text: {text["content"]}'},  {'role': 'assistant', 'content': "I’ve read this text."},  
+        {'role': 'user', 'content': f'what describes concepts in this text?'}],
+    stream=True,
+)
+
+for chunk in stream:
+  print(chunk['message']['content'], end='', flush=True)
+
+#if __name__ == '__main__':
+ # fire.Fire()
